a812fcf15d2c5eebd2a95ee58af35fb3fa3e6738
Dele <ajayibamidele60@gmail.com>
2023-11-27 15:50:17 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 11
 let sum = firstCard + secondCard
-let hasBlackJack = fasle
+let hasBlackJack = false
 let isAlive = true
+message = ""
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-    isAlive = false
+function startGame() {
+    if (sum <= 20) {
+        message= "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(message)
 }
+
+
+

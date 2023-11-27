4bc972b3c8dcc7c81cdb1ae68d7f7e36db548cec
Dele <ajayibamidele60@gmail.com>
2023-11-27 17:19:34 +0100

    new game function added

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-let firstCard = 10
-let secondCard = 11
+let firstCard = 9
+let secondCard = 9
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 message = ""
 
+let messageEl = document.getElementById("message-el")
+let sumEl =document.getElementById("sum-el")
+let CardsEl = document.getElementById("cards-el")
+
 function startGame() {
+    CardsEl.textContent = "Cards " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message= "Do you want to draw a new card? 🙂"
+        let message= "Do you want to draw a new card?"
+        messageEl.textContent = message
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        let message = "Wohoo! You've got Blackjack!"
+        messageEl.textContent = message
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        let message = "You're out of the game!"
+        messageEl.textContent = message
         isAlive = false
     }
     console.log(message)
 }
 
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+}
 
 
